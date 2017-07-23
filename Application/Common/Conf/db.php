077cb15ea661b63d693d16ca56a0e68af0d12ddb
Bob Zheng <bob@ultrarich.io>
2017-07-23 10:16:53 +0800

    version 0.01

---
+++ Application/Common/Conf/db.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bob
+ * Date: 2017/7/23
+ * Time: 17:56
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => '127.0.0.1',
+    'DB_USER' => 'root',
+    'DB_PWD' => 'Zheng1@06leavenotrace',
+    'DB_PORT' => 3306,
+    'DB_NAME' => 'chinesepie',
+    'DB_CHARSET' => 'utf8',
+    'DB_PREFIX' => 'cms_',
+);
